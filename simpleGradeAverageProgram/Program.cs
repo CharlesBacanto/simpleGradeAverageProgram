ea80cd5d9de4e14472ae3137c82a55c6ce14e5b3
charlesdominique <charlesdominiquebacanto@gmail.com>
2024-11-19 18:32:38 +0800

    Validating average inputs

--- simpleGradeAverageProgram/Program.cs
+++ simpleGradeAverageProgram/Program.cs
@@ -2,14 +2,21 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Please enter 4 grades");
-        double grade1 = Convert.ToDouble(Console.ReadLine());
-        double grade2 = Convert.ToDouble(Console.ReadLine());
-        double grade3 = Convert.ToDouble(Console.ReadLine());
-        double grade4 = Convert.ToDouble(Console.ReadLine());
+        ////Fetching Grades to compute
+        //Console.WriteLine("Please enter 4 grades");
+        //float grade1 = Convert.ToSingle(Console.ReadLine());
+        //float grade2 = Convert.ToSingle(Console.ReadLine());
+        //float grade3 = Convert.ToSingle(Console.ReadLine());
+        //float grade4 = Convert.ToSingle(Console.ReadLine());
 
-        Console.WriteLine("Input 1: " + grade1 + " Input 2: " + grade2 + " Input 3: " + grade3 + " Input 4: " + grade4);
-        double average = (grade1 + grade2 + grade3 + grade4) / 4;
+        ////Printing out Grades
+        //Console.WriteLine("Input 1: " + grade1 + " Input 2: " + grade2 + " Input 3: " + grade3 + " Input 4: " + grade4);
+
+        ////Arithmetic Computation
+        //float average = (grade1 + grade2 + grade3 + grade4) / 4;
+
+
+        float average = Convert.ToSingle(Console.ReadLine());// For validation of conditional statement
 
         if (average > 100)
         {
@@ -22,10 +29,10 @@
             Console.WriteLine(average + " is considered as With High Honors");
         } else if (average >= 90 || average == 94.99)
         {
-            Console.WriteLine(average + "is considered as With Honors");
+            Console.WriteLine(average + " is considered as With Honors");
         } else if (average >= 75 || average == 89.99)
         {
-            Console.WriteLine(average + "is considered as Passed");
+            Console.WriteLine(average + " is considered as Passed");
         } else if (average < 75)
         {
             Console.WriteLine(average + " is considered as Failed");
